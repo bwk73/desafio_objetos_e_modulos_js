b9494d4067028ffa45e29c67450ed71bae9536cd
Benjamin Wainer Kersz <bwkersz@gmail.com>
2023-06-30 10:49:11 -0300

    First commit

---
+++ js/services/order-service.js
@@ -0,0 +1,10 @@
+import * as shippingService from './shipping-service.js';
+
+export function total(order) {
+
+    const shipment = shippingService.shipment(order);
+    const discount = order.basic * order.discount / 100;
+
+    return order.basic + shipment - discount;
+
+}
